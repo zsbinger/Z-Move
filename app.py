b4f761e02c0516e59f352aaa6288bcb70b28cd89
Zachary Binger <zbinger@zacharys-air.home>
2021-02-13 20:45:38 -0800

    working on dynamic form updating-not finishe. committing before restructuring project

--- app.py
+++ app.py
@@ -2,14 +2,15 @@ __author__ = 'zsbinger'
 
 import os
 import datetime
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect, url_for, session, flash
 # request is a value that has something inside it whenever
 # we are in a function responding to a request (made by the browser)
 from pymongo import MongoClient
 from dotenv import load_dotenv
 
-load_dotenv()
+from forms import WorkoutForm
 
+load_dotenv()
 
 def create_app():
     # creates new Flask app and object with a unique name
@@ -20,6 +21,8 @@ def create_app():
         # serverSelectionTimeoutMS=3000  # 3 seconds
     )
     app.db = client.workouttracker
+
+    app.config['SECRET_KEY'] = 'bingers'
 
     @app.route("/")
     def home():
@@ -44,12 +47,19 @@ def create_app():
 
     @app.route("/creation", methods=["GET", "POST"])
     def creation():
-        if request.method == "POST":
-            entry_content = request.form.get("workout")
-            formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
-            app.db.workouts.insert({"content": entry_content, "date": formatted_date})
+        form = WorkoutForm()
+        if form.validate_on_submit():
+            flash('Your workout has been added.')
+            session['workout_content'] = form.workout_content.data
+            if request.method == "POST":
+                # save to mongodb database
+                workout_content = form.workout_content.data
+                formatted_date = datetime.datetime.today().strftime("%Y-%m-%d")
+                app.db.workouts.insert({"content": workout_content, "date": formatted_date})
 
-        return render_template("creation.html")
+            return redirect(url_for('creation'))
+
+        return render_template("creation.html", form=form)
 
     @app.route("/exercises")
     def exercises():
---
+++ forms.py
@@ -0,0 +1,22 @@
+from flask import render_template, session, redirect, url_for
+from flask_wtf import FlaskForm
+from wtforms import (StringField, BooleanField, IntegerField,
+                     SelectField, TextField, TextAreaField,
+                     DateField, SubmitField)
+from wtforms.validators import DataRequired
+
+class WorkoutForm(FlaskForm):
+    # name = StringField('Workout Name', validators=[DataRequired()])
+    name = StringField('Workout Name')
+    # add check to make sure that this is an integer
+    num_rounds = IntegerField()
+    scored_checkbox = BooleanField('Scored')
+
+    # will eventually be a dictionary to keep track of each component
+    # of the workout
+    workout_content = TextAreaField('Part A')
+    # add exercise drop down with list pulled from database
+
+    add_section_button = SubmitField('Add Section')
+    add_exercise_button = SubmitField('Add Exercise')
+    submit_workout_button = SubmitField(label='Submit Workout')
